0eb2351056c1037dd7209ff76f98a7f09c014c55
Alan-Miller <alan.ashman@gmail.com>
2017-11-02 19:00:51 -0600

    edits

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import socketIOClient from 'socket.io-client';
 const socket = socketIOClient('http://localhost:3001');
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor(prosp) {
+    super(prosp);
     this.state = {
       username: '',
       inputValue: '',
